ec276a386173154bf9268a41fbd8b503609985d5
Ticthach <thachdo1219@tamu.edu>
2023-03-27 00:08:38 -0500

    testing

--- backend/app.py
+++ backend/app.py
@@ -7,25 +7,34 @@ db = SQLAlchemy(app)
 
 class Custom(db.Model):
     itemID = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Float, nullable=False)
-    score = db.Column(db.Float, nullable=True)
-    fatTot = db.Column(db.Float, nullable=True)
-    fatSat = db.Column(db.Float, nullable=True)
-    fatTrans = db.Column(db.Float, nullable=True)
-    cholesterol = db.Column(db.Float, nullable=True)
-    sodium = db.Column(db.Float, nullable=True)
-    potassium = db.Column(db.Float, nullable=True)
-    carbTot = db.Column(db.Float, nullable=True)
-    dietaryFiber = db.Column(db.Float, nullable=True)
-    sugar = db.Column(db.Float, nullable=True)
-    protein = db.Column(db.Float, nullable=True)
-    vitA = db.Column(db.Float, nullable=True)
-    vitC = db.Column(db.Float, nullable=True)
-    calories = db.Column(db.Float, nullable=True)
-    iron = db.Column(db.Float, nullable=True)
+    itemName = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"Custom: {self.itemName}"
+
+    def __init__(self, itemName):
+        self.itemName = itemName
+# class Custom(db.Model):
+#     itemID = db.Column(db.Integer, primary_key=True)
+#     price = db.Column(db.Float, nullable=False)
+#     score = db.Column(db.Float, nullable=True)
+#     fatTot = db.Column(db.Float, nullable=True)
+#     fatSat = db.Column(db.Float, nullable=True)
+#     fatTrans = db.Column(db.Float, nullable=True)
+#     cholesterol = db.Column(db.Float, nullable=True)
+#     sodium = db.Column(db.Float, nullable=True)
+#     potassium = db.Column(db.Float, nullable=True)
+#     carbTot = db.Column(db.Float, nullable=True)
+#     dietaryFiber = db.Column(db.Float, nullable=True)
+#     sugar = db.Column(db.Float, nullable=True)
+#     protein = db.Column(db.Float, nullable=True)
+#     vitA = db.Column(db.Float, nullable=True)
+#     vitC = db.Column(db.Float, nullable=True)
+#     calories = db.Column(db.Float, nullable=True)
+#     iron = db.Column(db.Float, nullable=True)
     
 #     def __repr__(self):
-#         return f"Event: {self.itemID}"
+#         return f"Custom: {self.itemID}"
 
 #     def __init__(self, price, score, fatTot, fatSat, fatTrans, cholesterol, sodium, potassium, 
 #                  carbTot, dietaryFiber, sugar, protein, vitA, vitC, calories, iron):
@@ -46,24 +55,24 @@ class Custom(db.Model):
 #         self.calories = calories
 #         self.iron = iron
         
-class Custom(db.Model):
-    userID = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=True)
-    vegetarian = db.Column(db.Boolean, nullable=True)
-    vegan = db.Column(db.Boolean, nullable=True)
-    halal = db.Column(db.Boolean, nullable=True)
-    kosher = db.Column(db.Boolean, nullable=True)
-    glutenFree = db.Column(db.Boolean, nullable=True)
-    lactoseInt = db.Column(db.Boolean, nullable=True)
-    lowSodium = db.Column(db.Boolean, nullable=True)
-    lowCarb = db.Column(db.Boolean, nullable=True)
-    highProtien = db.Column(db.Boolean, nullable=True)
-    keto = db.Column(db.Boolean, nullable=True)
-    mushrooms = db.Column(db.Boolean, nullable=True)
-    seafood = db.Column(db.Boolean, nullable=True)
-    pickles = db.Column(db.Boolean, nullable=True)
+# class User(db.Model):
+#     userID = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, nullable=False)
+#     password = db.Column(db.String, nullable=False)
+#     email = db.Column(db.String, nullable=True)
+#     vegetarian = db.Column(db.Boolean, nullable=True)
+#     vegan = db.Column(db.Boolean, nullable=True)
+#     halal = db.Column(db.Boolean, nullable=True)
+#     kosher = db.Column(db.Boolean, nullable=True)
+#     glutenFree = db.Column(db.Boolean, nullable=True)
+#     lactoseInt = db.Column(db.Boolean, nullable=True)
+#     lowSodium = db.Column(db.Boolean, nullable=True)
+#     lowCarb = db.Column(db.Boolean, nullable=True)
+#     highProtien = db.Column(db.Boolean, nullable=True)
+#     keto = db.Column(db.Boolean, nullable=True)
+#     mushrooms = db.Column(db.Boolean, nullable=True)
+#     seafood = db.Column(db.Boolean, nullable=True)
+#     pickles = db.Column(db.Boolean, nullable=True)
     
 #     def __repr__(self):
 #         return f"Event: {self.userID}"
