5fdb2502fc30f857286bf47355e5f230ae34db2f
JiwonLeePortfolio <100000jilee@gmail.com>
2023-04-16 21:38:12 -0500

    reset appy.py

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,7 @@ import pprint
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:passwd@localhost/capstone'
 db = SQLAlchemy(app)
+engine = create_engine('postgresql://postgres:admin@localhost/capstone')
 CORS(app)
 
 # spoonacular api
@@ -63,32 +64,11 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User: {self.username}"
-    
-    def __init__(self, username, email, password, gender, weight_lbs, age, height_feet, height_inches, activity_level, vegitarian, vegan, halal, kosher, gluten_free, dairy_free, lactose_int, low_sodium, low_carb, high_protein, keto, paleo, preferences, restricitons):
+
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
-        self.gender = gender
-        self.weight_lbs = weight_lbs
-        self.age = age
-        self.height_feet = height_feet
-        self.height_inches = height_inches
-        self.activity_level=activity_level
-        self.vegitarian = vegitarian
-        self.vegan = vegan
-        self.halal = halal
-        self.kosher = kosher
-        self.gluten_free = gluten_free
-        self.dairy_free = dairy_free
-        self.lactose_int = lactose_int
-        self.low_sodium = low_sodium
-        self.low_carb = low_carb
-        self.high_protein = high_protein
-        self.keto = keto
-        self.paleo = paleo
-        self.preferences = preferences
-        self.restrictions = restricitons
-  
 
 class UserNutrition(db.Model):
 
@@ -177,8 +157,30 @@ def post_recipes():
 
 def format_user(user):
     return{
-        "userID":user.userID,
-        "username" :user.username
+        "userID": user.userID,
+        "username": user.username,
+        "email": user.email,
+        "password": user.password,
+        "gender": user.gender,
+        "weight_lbs": user.weight_lbs,
+        "age": user.age,
+        "height_feet": user.height_feet,
+        "height_inches": user.height_inches,
+        "activity_level": user.activity_level,
+        "vegitarian": user.vegitarian,
+        "vegan": user.vegan,
+        "halal": user.halal,
+        "kosher": user.kosher,
+        "gluten_free": user.gluten_free,
+        "dairy_free": user.dairy_free,
+        "lactose_int": user.lactose_int,
+        "low_sodium": user.low_sodium,
+        "low_carb": user.low_carb,
+        "high_protein": user.high_protein,
+        "keto": user.keto,
+        "paleo": user.paleo,
+        "preferences": user.preferences,
+        "restrictions": user.restrictions
     }
 
 # POST user information
@@ -909,3 +911,177 @@ def update_user():
     # user.update(dict(gender = gender))
     # db.session.commit()
     # return {'user': format_user(user.one())}
+
+
+# Returns requsted user from username
+@app.route('/getuserinfo', methods=['GET'])
+def get_user_info():
+    user = request.args.get('user')
+    result = db.session.query(User).filter_by(username=user)
+    users = []
+    for user in result:
+        users.append(format_user(user))
+    # formatted_user = format_user(result)
+    return {'user': users}
+
+@app.route('/events', methods=['POST'])
+def create_event1():
+    username = request.json['username']
+    event = User(username)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/username', methods=['POST'])
+def create_event_username():
+    username = request.json['username']
+    event = User(username)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST email
+@app.route('/email', methods=['POST'])
+def create_event_email():
+    email = request.json['email']
+    event = User(email)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST password
+@app.route('/password', methods=['POST'])
+def create_event_password():
+    password = request.json['password']
+    event = User(password)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/vegitarian', methods=['POST'])
+def create_event_vegitarian():
+    vegitarian = request.json['vegitarian']
+    event = User(vegitarian)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/vegan', methods=['POST'])
+def create_event_vegan():
+    vegan = request.json['vegan']
+    event = User(vegan)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/halal', methods=['POST'])
+def create_event_halal():
+    halal = request.json['halal']
+    event = User(halal)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST kosher
+@app.route('/kosher', methods=['POST'])
+def create_event_kosher():
+    kosher = request.json['kosher']
+    event = User(kosher)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/gluten_free', methods=['POST'])
+def create_event_gluten_free():
+    gluten_free = request.json['gluten_free']
+    event = User(gluten_free)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/dairy_free', methods=['POST'])
+def create_event_dairy_free():
+    dairy_free = request.json['dairy_free']
+    event = User(dairy_free)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/lactose_int', methods=['POST'])
+def create_event_lactose_int():
+    lactose_int = request.json['lactose_int']
+    event = User(lactose_int)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/low_sodium', methods=['POST'])
+def create_event_low_sodium():
+    low_sodium = request.json['low_sodium']
+    event = User(low_sodium)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/low_carb', methods=['POST'])
+def create_event_low_carb():
+    low_carb = request.json['low_carb']
+    event = User(low_carb)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/high_protein', methods=['POST'])
+def create_event_high_protein():
+    high_protein = request.json['high_protein']
+    event = User(high_protein)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+    # POST username
+@app.route('/keto', methods=['POST'])
+def create_event_keto():
+    keto = request.json['keto']
+    event = User(keto)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/paleo', methods=['POST'])
+def create_event_paleo():
+    paleo = request.json['paleo']
+    event = User(paleo)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/preferences', methods=['POST'])
+def create_event_preferences():
+    preferences = request.json['preferences']
+    event = User(preferences)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
+# POST username
+@app.route('/restrictions', methods=['POST'])
+def create_event_restrictions():
+    restrictions = request.json['restrictions']
+    event = User(restrictions)
+    db.session.add(event)
+    db.session.commit()
+    return format_user(event)
+
